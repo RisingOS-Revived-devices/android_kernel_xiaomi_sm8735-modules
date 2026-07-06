a48bed5d00d108d5acbc298f4ee2dfaca263490e
AdarshGrewal <adarshgrewal@gmail.com>
2026-07-06 14:36:54 +0530

    touch-drivers: nt36532: Manually trigger nvt_xm_htc_set_op_mode and nvt_set_extend_custom_cmd after fw update
     * it doesnt seem to enter raw mode automatically
    
    Change-Id: Ibf9cc63819f5def41b9eab520e5797a10cae23b2

--- qcom/opensource/touch-drivers/nt36532/nt36xxx_fw_update.c
+++ qcom/opensource/touch-drivers/nt36532/nt36xxx_fw_update.c
@@ -987,6 +987,13 @@ download_fail:
 request_firmware_fail:
 #if TOUCH_THP_SUPPORT
 	ts->xm_htc_sw_reset = false;
+	if (ret == 0 && ts->enable_touch_raw) {
+		extern int32_t nvt_xm_htc_set_op_mode(int16_t op_mode);
+		extern int32_t nvt_set_extend_custom_cmd(uint8_t sub_cmd,
+							 int16_t value);
+		nvt_xm_htc_set_op_mode(2);
+		nvt_set_extend_custom_cmd(0x0A, 120);
+	}
 #endif
 	return ret;
 }
